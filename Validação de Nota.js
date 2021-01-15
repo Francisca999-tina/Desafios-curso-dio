9c7dbdbd2ccd12630ebd380e4b85652c3ab52a74
Fran Costa <68696276+Francisca999-tina@users.noreply.github.com>
2021-01-14 23:09:15 -0300

    Update Validação de Nota.js

--- Validação de Nota.js
+++ Validação de Nota.js
@@ -15,3 +15,34 @@ while (n2 > 10 || n2 < 0 ) {
 let m = (n1 + n2)/2 ; 
 
 console.log("media = " + m.toFixed(2));
+
+###########################################################################################################
+
+using System;
+
+class Desafio {
+  static void Main() {
+    double doCalc = 1;
+    while (doCalc == 1) 
+    {
+      double numbersValids = 0;
+      float total = 0;
+      while (numbersValids < 2) 
+      {
+        float grade = float.Parse(Console.ReadLine());
+        if (grade >= 0 && grade <= 10)
+        {
+          numbersValids ++;
+          total += grade;
+        } else {
+          Console.WriteLine("nota invalida");
+        }
+      }
+      Console.WriteLine($"media = {(total / 2).ToString("n2")}");
+      do {
+        Console.WriteLine("novo calculo (1-sim 2-nao)");
+        doCalc = double.Parse(Console.ReadLine());
+      } while (doCalc !=  1 && doCalc != 2);
+    }
+  }
+}
