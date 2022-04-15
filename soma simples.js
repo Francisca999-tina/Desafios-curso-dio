8c8e0040333f7f7803e1c431bee8c53e3ac0121f
Fran Costa <68696276+Francisca999-tina@users.noreply.github.com>
2022-04-15 13:00:08 -0300

    Update soma simples.js

--- soma simples.js
+++ soma simples.js
@@ -2,3 +2,12 @@ let valor1 = parseInt(gets());
 let valor2 = parseInt(gets());
 let total = valor1 + valor2;
 console.log("SOMA = " + total);
+
+
+//resoluçao 2
+var A = parseInt(gets());
+var B = parseInt(gets());
+
+let soma = (A + B);
+
+console.log("SOMA = " + soma);
