60e25f9f36b5ad723e217bd1e63ebba3d3d3a96a
Fran Costa <68696276+Francisca999-tina@users.noreply.github.com>
2022-04-15 11:53:59 -0300

    Update O Escolhido.js

--- O Escolhido.js
+++ O Escolhido.js
@@ -16,3 +16,34 @@ classe.forEach(function(value, key) {
 }, classe);
 
 console.log(o_escolhido[1] >= 8 ? o_escolhido[0] : 'Minimum note not reached');
+
+
+
+//solução 2
+let qntAlunos = parseInt(gets());
+let matricula = Array(qntAlunos);
+let nota = Array(qntAlunos);
+
+for (i = 0; i < qntAlunos; i++) {
+   let aluno = gets().split(' ');
+    matricula[i] = parseInt(aluno[0]);
+    nota[i] = parseFloat(aluno[1]);
+
+}
+
+let notaMax = nota.reduce( (a, b) => {
+   return Math.max(a, b);
+
+});
+
+for (i = 0; i < qntAlunos; i++) {
+   if (notaMax <= 8) {
+      print('Minimum note not reached');
+      break;
+
+   } else if (nota[i] === notaMax) {
+     print(matricula[i]);
+
+  }
+
+}
